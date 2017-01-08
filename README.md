7b637c63907aebee921a6f556270fe21fe70c94f
Jane Hauf <janehauf@gmail.com>
2017-01-07 21:54:06 -0800

    update readme

---
+++ README.md
@@ -0,0 +1,27 @@
+###StockTracker
+
+#This is a web scraper that tracks changes to the S&P 500's price from CNN using JavaScript and jQuery. Each second, it makes an AJAX GET request to CNN Money and pulls the current price. It checks that price against the last price in the table and updates the table if the price changed.
+
+
+`js
+
+function pullPrice() {
+  $.ajax({
+      url: 'http://money.cnn.com/data/markets/sandp/',
+      type: 'GET',
+      success: function(res) {
+          let result = $(res.responseText).find(".wsod_last").find("span")[0].innerHTML;
+          let container = document.getElementById("container");
+          if (prices.length < 1 || prices[prices.length - 1].result !== result) {
+            let totalDate = new Date().toString().replace(" ", ", ").replace("GMT-0800", "")
+            let date = totalDate.slice(0,-15);
+            let time = totalDate.slice(-15);
+            prices.push({result: result, date: date, time: time});
+            container.innerHTML = `<span>` + `<li>Price</li>` + prices.map((price) => `<li>$${price.result}</li>`).join(" ") + `</span> <span>` +`<li>Date</li>` + prices.map((price) => `<li>${price.date}</li>`).join(" ") + `</span> <span>` +`<li>Time</li>` + prices.map((price) => `<li>${price.time}</li>`).join(" ") + `</span>`;
+          }
+      }
+  });
+}
+
+setInterval(pullPrice, 1000);
+`
--- test.js
+++ test.js
@@ -5,22 +5,14 @@ jQuery.ajax = (function(_ajax){
         exRegex = RegExp(protocol + '//' + hostname),
         YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
         query = 'select * from html where url="{URL}" and xpath="*"';
-
     function isExternal(url) {
         return !exRegex.test(url) && /:\/\//.test(url);
     }
-
     return function(o) {
-
         var url = o.url;
-
         if ( /get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url) ) {
-
-            // Manipulate options so that JSONP-x request is made to YQL
-
             o.url = YQL;
             o.dataType = 'json';
-
             o.data = {
                 q: query.replace(
                     '{URL}',
@@ -30,36 +22,24 @@ jQuery.ajax = (function(_ajax){
                 ),
                 format: 'xml'
             };
-
-            // Since it's a JSONP request
-            // complete === success
             if (!o.success && o.complete) {
                 o.success = o.complete;
                 delete o.complete;
             }
-
             o.success = (function(_success){
                 return function(data) {
-
                     if (_success) {
-                        // Fake XHR callback.
                         _success.call(this, {
                             responseText: (data.results[0] || '')
-                                // YQL screws with <script>s
-                                // Get rid of them
                                 .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
                         }, 'success');
                     }
 
                 };
             })(o.success);
-
         }
-
         return _ajax.apply(this, arguments);
-
     };
-
 })(jQuery.ajax);
 
 let prices = [];
@@ -77,7 +57,6 @@ function pullPrice() {
             let time = totalDate.slice(-15);
             prices.push({result: result, date: date, time: time});
             container.innerHTML = `<span>` + `<li>Price</li>` + prices.map((price) => `<li>$${price.result}</li>`).join(" ") + `</span> <span>` +`<li>Date</li>` + prices.map((price) => `<li>${price.date}</li>`).join(" ") + `</span> <span>` +`<li>Time</li>` + prices.map((price) => `<li>${price.time}</li>`).join(" ") + `</span>`;
-
           }
       }
   });
